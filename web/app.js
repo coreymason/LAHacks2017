081eb7d41381498c993af1ccf8fe7dd5378384b4
Corey Mason <corey.mason98@googlemail.com>
2017-04-01 06:34:02 -0700

    Dream Log Recieve, Process, and Submit to DB
    Untested

--- web/app.js
+++ web/app.js
@@ -1,9 +1,21 @@
 var express = require('express'),
-	app = express(),
-	routes = require('./routes');;
+	bodyParser = require('body-parser'),
+	rp = require('request-promise'),
+	firebase = require("firebase"),
+	secrets = require("./secrets"),
+	routes = require('./routes'),
+	app = express();
 
+firebase.initializeApp(secrets.fbconfig);
+
+var database = firebase.database;
 var port = process.env.PORT || 8080;
 
+app.use(bodyParser.json());
+
 app.get('/', routes.home);
+app.get('/suggestions', routes.suggestions);
+app.post('/roomData', routes.roomData);
+app.post('/dreamLog', routes.dreamLog);
 
 app.listen(port);
--- web/routes.js
+++ web/routes.js
@@ -1,5 +1,68 @@
 module.exports = {
-	home: function () {
+	home: function() {
 		// body...
-	}
+	},
+
+	suggestions: function(req, res) {
+	},
+
+	roomData: function(req, res) {
+	},
+
+	dreamLog: function(req, res) {
+		if(!req.body) {
+			//nothing sent
+			res.sendStatus(400);
+		} else {
+			var sentiment, keywords;
+			//send to microsoft nlp
+			var optionsSent = {
+		    method: 'POST',
+		    uri: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
+		    body: {
+		        data: {'documents': ['language': 'en', 'id': '0', 'text': req.body.text]}
+		    },
+				headers: {
+					'Ocp-Apim-Subscription-Key': secrets.cog
+				},
+		    json: true
+			};
+			rp(optionsSent).then(function(body) {
+				sentiment = body.documents.score;
+			}).catch(function(err) {
+				res.status(500).send(err);
+			});
+
+			var optionsKey = {
+		    method: 'POST',
+		    uri: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
+		    body: {
+		        data: {'documents': ['language': 'en', 'id': '0', 'text': req.body.text]}
+		    },
+				headers: {
+					'Ocp-Apim-Subscription-Key': secrets.cog
+				},
+		    json: true
+			};
+			rp(optionsKey).then(function(body) {
+				keywords = body.documents.keyPhrases;
+			}).catch(function(err) {
+				res.status(500).send(err);
+			});
+		}
+
+		//send to database
+		var newDreamLog = {
+			date = new Date(),
+			dreamContent = req.body.text,
+			keywords = kewords,
+			sentiment = sentiment
+		}
+		database.ref('users/' + userId).child('dreamLogs').push(newDreamLog).then(function() {
+			res.sendStatus(200);
+		}).catch(function(err) {
+			res.status(500).send(err);
+		});
+	},
+
 }
