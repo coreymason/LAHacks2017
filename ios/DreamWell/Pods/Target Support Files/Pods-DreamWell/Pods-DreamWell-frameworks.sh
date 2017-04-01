22f8fa39f7e1856f8c2c9fa0e3f5243b33f14912
rohinb <rohinbhushan@gmail.com>
2017-04-01 13:06:22 -0700

    add AlamofireSwiftyJSON for communicating through REST API with server

--- ios/DreamWell/Pods/Target Support Files/Pods-DreamWell/Pods-DreamWell-frameworks.sh
+++ ios/DreamWell/Pods/Target Support Files/Pods-DreamWell/Pods-DreamWell-frameworks.sh
@@ -89,12 +89,18 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/AlamofireSwiftyJSON/AlamofireSwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Charts/Charts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/AlamofireSwiftyJSON/AlamofireSwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Charts/Charts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
