37d8ff891b39f62fef286b58eaab7745af3ca6f9
rohinb <rohinbhushan@gmail.com>
2017-04-01 19:48:43 -0700

    Fix up server calls and add to ui

--- ios/DreamWell/DreamWell/Services.swift
+++ ios/DreamWell/DreamWell/Services.swift
@@ -9,41 +9,25 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
-import AlamofireSwiftyJSON
 
 struct Services {
 	static let userID = "SwFaJKpAMIaKhIz4vS4FCnYvTj33"
-	static let root = "localhost:8080"
-	static func postSleepLogs(text: String) {
-		let todosEndpoint: String = "\(root)/dreamLog/\(userID)"
-		let newLog: [String: Any] = ["text": text]
-		Alamofire.request(todosEndpoint, method: .post, parameters: newLog,
-							encoding: JSONEncoding.default)
-			.responseJSON { response in
-		guard response.result.error == nil else {
-			// got an error in getting the data, need to handle it
-			print("error calling POST on dream logs")
-			print("request", response.request)
-			print(response.result.error!)
-			return
+	static let root = "http://projectscale.me"
+	static func postSleepLogs(text: String, stars: Int) {
+		let todoEndpoint: String = "\(root)/dreamLog/\(userID)"
+		let newTodo: [String: Any] = ["text" : text, "quality" : stars]
+		Alamofire.request(todoEndpoint, method: .post, parameters: newTodo,
+                    encoding: JSONEncoding.default).responseString { response in
+			guard response.result.error == nil else {
+				// got an error in getting the data, need to handle it
+				print("error calling POST on dreamlog")
+				print(response.result.error!)
+				return
+			}
+			print("SUCCESS SENDING SLEEP LOGS")
 		}
-		// make sure we got some JSON since that's what we expect
-		guard let json = response.result.value as? [String: Any] else {
-			print("didn't get todo object as JSON from API")
-			print("Error: \(response.result.error)")
-			return
-		}
-		// get and print the title
-		guard let todoTitle = json["title"] as? String else {
-			print("Could not get todo title from JSON")
-			return
-		}
-		print("The title is: " + todoTitle)
-  }
 	}
-	
 }
-
 
 
 
