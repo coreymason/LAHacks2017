353073e9c1e40d65c80e8ed878bf2502eae80bf9
Pranay Jain <pranay1117@yahoo.co.in>
2017-04-02 09:01:48 -0700

    improve python script

--- web/linear_regression_engine.py
+++ web/linear_regression_engine.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 reg = skl.LinearRegression()
 
 data = pd.read_csv('sleep_quality_data.csv', index_col=0)
-x_train = data.as_matrix(['temperature', 'humidity', 'brightness'])[:150]
-y_train = data.as_matrix(['sleep quality'])[:150]
+x_train = data.as_matrix(['temperature', 'humidity', 'brightness'])[:13]
+y_train = data.as_matrix(['sleep quality'])[:13]
 
 
 reg.fit (x_train, y_train)
@@ -43,16 +43,39 @@ for cof in reg.coef_[0]:
 
 #print suggestion
 
-x_test = data.as_matrix(['temperature', 'humidity', 'brightness'])[-50:]
+x_test = data.as_matrix(['temperature', 'humidity', 'brightness'])[-1:]
 #print x_test
 
-preds = reg.predict(x_test)
-#print preds
+predicted_value = reg.predict(x_test)
+print predicted_value
+# if predicted_value < 3:
+# 	for cof in reg.coef_[0]:
+# 	suggestion = ""
+# 	if cof > 0.5:
+# 		suggestion += "increase " + fields[index] + ", "
+# 		print suggestion
+# 		index += 1
+# 	elif cof > 0: 
+# 		suggestion += "slightly increase " + fields[index] + ", "
+# 		print suggestion
+# 		index += 1
+# 	elif cof < -0.5:
+# 		suggestion += "decrease " + fields[index] + ", "
+# 		print suggestion
+# 		index += 1
+# 	elif cof < 0:
+# 		suggestion += "slightly decrease " + fields[index] + ", "
+# 		print suggestion
+# 		index+=1
+# 	else:
+# 		suggestion += "it's fine " + ", "
+# 		print suggestion
+# 		index+=1
 
 # plot data
 data.plot(kind='scatter', x='temperature', y='sleep quality')
 
 # plot the least squares line
-plt.plot(x_test, preds, c='red', linewidth=2)
+plt.plot(x_test, predicted_value, c='red', linewidth=2)
 
 #plt.show()
